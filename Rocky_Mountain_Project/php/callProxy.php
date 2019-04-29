45b9cf89f9e3a9f058c7855ac53a467c640f4250
willoughby2 <willoughby2@wisc.edu>
2019-04-29 13:46:28 -0500

    Update callProxy.php

--- Rocky_Mountain_Project/php/callProxy.php
+++ Rocky_Mountain_Project/php/callProxy.php
@@ -1,5 +1,5 @@
 <?php
-    include 'C:\xampp\htdocs\cartodbProxy.php';
+    include '\cartodbProxy.php';
     //          ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
     $queryURL = $_POST['qurl'];
     $return = goProxy($queryURL);
